0e2d8bd4c2562291e4482d38ff0cc39f4f84e9a3
Stepan Babakov <stepan.babakov.1999@mail.ru>
2021-04-18 17:33:28 +0300

    Реализация 3 задания через if/elsif

--- Ruby_1.rb
+++ Ruby_1.rb
@@ -1,1 +1,24 @@
-print "Hello world"
+# 1 Задание
+puts ("Hello world")
+# 2 Задание
+username = "Stepan"
+#printf("Привет, %s!", username)
+# 3 задание
+printf("Какой твой любимый язык программирования, %s?\n", username)
+lang = gets.chomp.strip
+lang.capitalize!
+if lang == "Ruby"
+  puts "Подлиза)))))"
+elsif lang == "Python"
+  puts "Нормально, нормально. Но скоро будет Ruby"
+elsif lang == "C++"
+  puts "Жесть. Но скоро будет Ruby)"
+elsif lang == "R"
+  puts "Статист значит. Но скоро будет Ruby"
+elsif lang == "JS"
+  puts "Извращюга. Но Ruby лучше)"
+elsif lang == "C#"
+  puts "Скоро будет Ruby"
+elsif lang == "PHP"
+  puts "ъеъ. Skoro budet Руби"
+end
