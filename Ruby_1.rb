0090430108530aaadc3fca76be6a6ea67403676a
Stepan Babakov <stepan.babakov.1999@mail.ru>
2021-04-19 13:41:55 +0300

    Реализация 4 задания

--- Ruby_1.rb
+++ Ruby_1.rb
@@ -4,24 +4,31 @@ puts ("Hello world")
 username = "Stepan"
 #printf("Привет, %s!", username)
 # 3 задание
-printf("Какой твой любимый язык программирования, %s?\n", username)
-lang = gets.chomp.strip
-lang.capitalize!
-case lang
-  when "Ruby"
-  puts "Подлиза)))))"
-  when "Python"
-  puts "Нормально, нормально. Но скоро будет Ruby"
-  when "C++"
-  puts "Жесть. Но скоро будет Ruby)"
-  when "R"
-  puts "Статист значит. Но скоро будет Ruby"
-  when "JS"
-  puts "Извращюга. Но Ruby лучше)"
-  when "C#"
-  puts "Скоро будет Ruby"
-  when "PHP"
-  puts "ъеъ. Skoro budet Руби"
-else
-  puts "Такого языка нет("
-end
+# printf("Какой твой любимый язык программирования, %s?\n", username)
+# lang = gets.chomp.strip
+# lang.capitalize!
+# case lang
+#   when "Ruby"
+#   puts "Подлиза)))))"
+#   when "Python"
+#   puts "Нормально, нормально. Но скоро будет Ruby"
+#   when "C++"
+#   puts "Жесть. Но скоро будет Ruby)"
+#   when "R"
+#   puts "Статист значит. Но скоро будет Ruby"
+#   when "JS"
+#   puts "Извращюга. Но Ruby лучше)"
+#   when "C#"
+#   puts "Скоро будет Ruby"
+#   when "PHP"
+#   puts "ъеъ. Skoro budet Руби"
+# else
+#   puts "Такого языка нет("
+# end
+# 4 задание
+puts "Введите команду на Ruby!"
+intput = gets.chomp.strip
+eval intput
+puts "Введите командную строку своей ОС!"
+input = gets.chomp.strip
+system input
