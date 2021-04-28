bbef5357f05fe9c3dd267121e8e4ae381111b6b3
Stepan Babakov <stepan.babakov.1999@mail.ru>
2021-04-28 20:31:05 +0300

    Реализация 6-го и 7-го(1 метод) задания

--- Ruby_1.rb
+++ Ruby_1.rb
@@ -33,9 +33,79 @@ username = "Stepan"
 # input = gets.chomp.strip
 # system input
 # 5 задание работа с числами
-number = gets.chomp
-sum = 0
-number.to_s.each_char do |num|
-  sum += num.to_i
+# number = gets.chomp
+# sum = 0
+# number.to_s.each_char do |num|
+#   sum += num.to_i
+# end
+# print sum
+# sum_char = lambda { |number, sum = 0| number.to_s.each_char do |num| sum += num.to_i end
+# sum }
+# number = gets.chomp
+# puts sum_char.call(number)
+# 6 задание
+
+def sum_char str
+  sum = 0
+  str.to_s.each_char do |num|
+    sum += num.to_i
+  end
+  sum
 end
-print sum
+
+def multiplex_char str
+  sum = 1
+  str.to_s.each_char do |num|
+    sum *= num.to_i
+  end
+  sum
+end
+
+def max_char str
+  max = 0
+  str.to_s.each_char do |num|
+    if max < num.to_i
+      max = num.to_i
+    end
+  end
+  max
+end
+
+def min_char str
+  min = 10
+  str.to_s.each_char do |num|
+    if min < num.to_i
+      min = num.to_i
+    end
+  end
+  min
+end
+
+def just str, num = 2
+  if str < 2
+    return false
+  elsif str == 2
+    return true
+  elsif str % num == 0
+    return false
+  elsif num < str / 2
+    just(str, num + 1)
+  else
+    return true
+  end
+end
+
+def just_number str, num = 2, value = 0
+  if str / 2 >= num
+    if str % num == 0 && just(num)
+      just_number(str, num + 1, value + 1)
+    else
+      just_number(str, num + 1, value)
+    end
+  else
+    return value
+  end
+end
+
+str = gets.chomp
+puts just_number(str.to_i)
--- Ruby_test.rb
+++ Ruby_test.rb
@@ -1,2 +1,73 @@
-list = [1, 2, 3, 4, 5]
-list.each {|i| puts i}
+require 'bigdecimal'
+#определяем два новых класса исключений
+class OvenOffError < StandardError
+end
+
+class OvenEmptyError < StandardError
+end
+
+class SmallOven
+
+  attr_accessor :contents
+
+  def turn_on
+    puts "Turning oven on."
+    @state = "on"
+  end
+
+  def turn_off
+    puts "Turning oven off."
+    @state = "off"
+  end
+
+  def bake
+    unless @state == "on"
+      # инициируем исключение одного типа, если духовка выключена...
+      raise OvenOffError, "You need to turn the oven on first!"
+    end
+    if @contents == nil
+      # ...и исключение другого типа, если духовка пуста.
+      raise OvenEmptyError, "There's nothing in the oven!"
+    end
+    "golden-brown #{contents}"
+  end
+end
+
+dinner = ['turkey', nil, 'pie']
+oven = SmallOven.new
+oven.turn_off
+dinner.each do |item|
+  begin
+    oven.contents = item
+    puts "Serving #{oven.bake}."
+    # Перехватываем только исключение OvenEmptyError.
+  rescue OvenEmptyError => error
+    # Выводим сообщение из исключения как и в предыдущей версии кода.
+    puts error.message
+    # Перехватываем только исключение OvenOffErrors.
+  rescue OvenOffError => error
+    # Раз духовка выключена мы вкючаем её.
+    oven.turn_on
+    # После включение духовки мы возвращаем управление к началу блока "begin"
+    retry
+  ensure
+    oven.turn_off
+  end
+end
+
+# class Person
+#   # def name
+#   #   @name
+#   # end
+#   #
+#   # def name=(str)
+#   #   @name = str
+#   # end
+#   attr_accessor :name
+#   def greeting
+#     "Hello #{@name}"
+#   end
+# end
+#
+# person = Person.new
+# puts person.name = 'Dennis'
